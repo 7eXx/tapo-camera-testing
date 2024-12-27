191bb75f0a52f9511caa6ea12877742a1880f65f
7eXx <marco.tex@live.com>
2024-12-27 17:29:51 +0100

    add method to away mode and home mode

--- main.py
+++ main.py
@@ -7,5 +7,33 @@ config = {
     **dotenv_values(".env.local")
 }
 
+tapo = Tapo(config["TAPO_IP"], config["TAPO_USERNAME"], config["TAPO_PASSWORD"])
+
+
+
+def set_home_mode() -> None:
+    tapo.setMotionDetection(False)
+    tapo.setPersonDetection(False)
+    tapo.setTamperDetection(False)
+    tapo.setAlarm(False)
+    tapo.setNotificationsEnabled(False)
+    tapo.setPrivacyMode(True)
+
+def set_away_mode() -> None:
+    tapo.setMotionDetection(True)
+    tapo.setPersonDetection(True)
+    tapo.setTamperDetection(True)
+    tapo.setAlarm(True)
+    tapo.setNotificationsEnabled(True)
+    tapo.setPrivacyMode(False)
+
 if __name__ == '__main__':
+    privacy_mode = tapo.getPrivacyMode()
+    print(f"Privacy mode: {privacy_mode}")
+
+    motion = tapo.getMotionDetection()
+    print(f"Motion detection: {motion}")
+
+    alarm = tapo.getAlarm()
+    print(f"Alarm: {alarm}")
 
